52ad030d82f1d0b4730a237345b3de47e356082c
molligan <molligan@gmail.com>
2015-04-08 18:19:16 -0400

    Update

--- app/models/encounter.js
+++ app/models/encounter.js
@@ -3,6 +3,8 @@ import DS from "ember-data";
 var Encounter = DS.Model.extend({
 	type: DS.attr('String'),
 	dateOfEncounter: DS.attr('string'),
+	cnIILeft: DS.attr('string'),
+	cnIIRight: DS.attr('string'),
 	patient: DS.belongsTo('patient', {async: true}),
 	pathologies: DS.hasMany('pathology', {async: true})
 });
